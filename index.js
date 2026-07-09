75f8c4dd459af6b367b168238e742f0448610e15
anas0456 <anasforahmad@gmail.com>
2026-07-09 08:56:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const { Client } = require('discord.js-selfbot-v13');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const http = require('http');
+
 const client = new Client();
 
-client.on('ready', () => {
+// هذا الجزء يمنع الـ Crash في Railway
+http.createServer((req, res) => {
+    res.write("I am alive");
+    res.end();
+}).listen(process.env.PORT || 8080);
+
+client.on('ready', async () => {
     console.log(`تم تسجيل الدخول كـ ${client.user.tag}`);
     
-    // هنا نحدد الروم الصوتي
-    const channel = client.channels.cache.get('1496674843184074945');
+    const channelId = '1496674843184074945';
+    const channel = client.channels.cache.get(channelId);
+    
     if (channel) {
-        channel.join().then(() => {
+        try {
+            joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+                selfDeaf: true,
+                selfMute: true
+            });
             console.log("تم الانضمام للروم بنجاح!");
-        }).catch(err => console.log("خطأ في الانضمام: " + err));
+        } catch (err) {
+            console.error("خطأ في الاتصال الصوتي:", err);
+        }
+    } else {
+        console.log("لم يتم العثور على الروم!");
     }
 });
 
