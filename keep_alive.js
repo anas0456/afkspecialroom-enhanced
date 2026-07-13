1a6f8f4ecdd73c300bbeca681c2b2f3da0346014
anas0456 <anasforahmad@gmail.com>
2026-07-13 05:16:46 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,10 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running...');
+});
+
+server.listen(3000, () => {
+  console.log('Keep alive server running on port 3000');
+});
